9b237479b9ef3121f1407134357ee2f983ce5b66
Alex Sanchez <aa.sanchez.dev@gmail.com>
2024-08-22 13:26:21 -0400

    updated the item and app component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ curName, setCurCat ] = useState("")
+
+  const newClassName = curName ? "in-cart" : "";
+
+  function handleClick(event){
+    setCurCat(!curName)
+  }
+
   return (
-    <li className="">
+    <li onClick= {handleClick} className= {newClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add">{curName ? "Remove From Cart" : "Add to Cart" }</button>
     </li>
   );
 }
